f203e6a30c969444d206443fc6d4cb7f480360e7
Geoffroy <gefthefrench@gmail.com>
2015-05-19 22:34:52 -0500

    Added functionType

---
+++ malgol/type/FunctionType.java
@@ -0,0 +1,49 @@
+package malgol.type;
+
+import java.util.LinkedList;
+
+import malgol.ast.Expression;
+
+public class FunctionType extends Type {
+	private LinkedList<Expression> paramTypes;
+	private int numParams;
+	private Type returnType;
+
+	public FunctionType(int size) {
+		super(-1);
+	}
+	
+	public LinkedList<Expression> getParamTypes(){
+		return paramTypes;
+	}
+	
+	public int getNumParams() {
+		return numParams;
+	}
+	
+	public Type getReturnType() {
+		return returnType;
+	}
+
+	@Override
+	public boolean equals(Type t2) {
+		if (!(t2 instanceof FunctionType)) {
+			return false;
+		}
+		FunctionType at2 = (FunctionType) t2;
+		return 	   (numParams == at2.numParams) 
+				&& (paramTypes.equals(at2.paramTypes)) 
+				&& (returnType.equals(at2.returnType));
+	}
+
+	@Override
+	public boolean isFunction() {
+		return true;
+	}
+
+	@Override
+	public Type baseType() {
+		return ((FunctionType) this).baseType();
+	}
+
+}
